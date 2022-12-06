7960e792589ae31d21167cb36aaa9b0f3c873142
Lucas Leite <lucas.leitedosreis@gmail.com>
2022-12-06 16:18:08 -0300

    feat: Adicionando função para pegar click do mouse, e exibindo na tela

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,30 @@ interface ClickedProps {
 function App() {
   const [clickedPoints, setClickedPoints] = useState<ClickedProps[]>([]);
 
+  //* Função para pegar as cordenadas onde foi clicado com o mouse
   function getCordenatesClick(e: React.MouseEvent<HTMLElement>) {
     const { clientX, clientY } = e;
+    //** Vai armazenar as cordenadas com useState em um array
     setClickedPoints([...clickedPoints, { clientX, clientY }]);
   }
-  return <div className="App" onClick={getCordenatesClick}></div>;
+  return (
+    <div className="App" onClick={getCordenatesClick}>
+      {clickedPoints.map((clickedPoint, index) => {
+        return (
+          <div
+            key={index}
+            style={{
+              position: "absolute",
+              left: clickedPoint.clientX,
+              top: clickedPoint.clientY,
+            }}
+          >
+            0
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
