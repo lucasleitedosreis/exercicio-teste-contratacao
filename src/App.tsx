398db8fe0f0caaaacb1281b5a1fa681613dc01ca
Lucas Leite <lucas.leitedosreis@gmail.com>
2022-12-07 18:01:02 -0300

    feat: Criando e adicionando a função handleUndoClick

--- src/App.tsx
+++ src/App.tsx
@@ -14,23 +14,40 @@ function App() {
     //** Vai armazenar as cordenadas com useState em um array
     setClickedPoints([...clickedPoints, { clientX, clientY }]);
   }
+
+  //* Função para remover o o ponto adicionado pelo click
+  function handleUndoClick() {
+    //* cria uma cópia do array clickedPoints
+    const newClickedPoints = [...clickedPoints];
+    //* Com método pop remove o último point do array
+    newClickedPoints.pop();
+    //*Adiciona novamente ao state o novo array modificado
+    setClickedPoints(newClickedPoints);
+  }
   return (
-    <div className="App" onClick={getCordenatesClick}>
-      {clickedPoints.map((clickedPoint, index) => {
-        return (
-          <div
-            key={index}
-            style={{
-              position: "absolute",
-              left: clickedPoint.clientX,
-              top: clickedPoint.clientY,
-            }}
-          >
-            0
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <button disabled={clickedPoints.length === 0} onClick={handleUndoClick}>
+        Undo
+      </button>
+      <div className="App" onClick={getCordenatesClick}>
+        {clickedPoints.map((clickedPoint, index) => {
+          return (
+            <div
+              key={index}
+              style={{
+                width: "20px",
+                height: "20px",
+                position: "absolute",
+                background: "blue",
+                borderRadius: "50%",
+                left: clickedPoint.clientX - 8,
+                top: clickedPoint.clientY - 9,
+              }}
+            ></div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
