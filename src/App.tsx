77b5065afd7c393b8900afc2cad31cef995cf238
Lucas Leite <lucas.leitedosreis@gmail.com>
2022-12-07 18:38:02 -0300

    feat: Criando e adicionando a função handleRedoClick

--- src/App.tsx
+++ src/App.tsx
@@ -7,28 +7,49 @@ interface ClickedProps {
 }
 function App() {
   const [clickedPoints, setClickedPoints] = useState<ClickedProps[]>([]);
+  const [undoPoints, setUndoPoints] = useState<ClickedProps[]>([]);
 
   //* Função para pegar as cordenadas onde foi clicado com o mouse
   function getCordenatesClick(e: React.MouseEvent<HTMLElement>) {
     const { clientX, clientY } = e;
-    //** Vai armazenar as cordenadas com useState em um array
+    // Vai armazenar as cordenadas com useState em um array
     setClickedPoints([...clickedPoints, { clientX, clientY }]);
   }
 
-  //* Função para remover o o ponto adicionado pelo click
+  //* Função para remover o ponto adicionado pelo click
   function handleUndoClick() {
-    //* cria uma cópia do array clickedPoints
+    // cria uma cópia do array clickedPoints
     const newClickedPoints = [...clickedPoints];
-    //* Com método pop remove o último point do array
-    newClickedPoints.pop();
-    //*Adiciona novamente ao state o novo array modificado
+    // Com método pop remove o último point do array
+    const undoPoint = newClickedPoints.pop();
+    //Adiciona novamente ao state o novo array modificado
     setClickedPoints(newClickedPoints);
+    //verifica se undoPoint esta vazio
+    if (!undoPoint) return;
+    // Adiciona o ponto que foi removido a um novo state
+    setUndoPoints([...undoPoints, undoPoint]);
   }
+
+  //* Função para refazer o ponto adicionado pelo click
+  function handleRedoClick() {
+    // cria uma cópia do array undoPoints
+    const newUndoPoints = [...undoPoints];
+    // Com método pop remove o último point do array e armazena em redoPoint
+    const redoPoint = newUndoPoints.pop();
+
+    setUndoPoints(newUndoPoints);
+    //verifica se redoPoint esta vazio
+    if (!redoPoint) return;
+    // Adiciona novamente o ponto que foi removido ao array
+    setClickedPoints([...clickedPoints, redoPoint]);
+  }
+
   return (
     <>
       <button disabled={clickedPoints.length === 0} onClick={handleUndoClick}>
         Undo
       </button>
+      <button onClick={handleRedoClick}>Redo</button>
       <div className="App" onClick={getCordenatesClick}>
         {clickedPoints.map((clickedPoint, index) => {
           return (
